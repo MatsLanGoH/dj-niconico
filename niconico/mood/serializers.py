f67bf6a851f948909cb590f045beb6aa90b6874e
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-08-01 06:22:27 +0900

    Remove Field

--- niconico/mood/serializers.py
+++ niconico/mood/serializers.py
@@ -5,7 +5,6 @@ from .models import Mood
 
 class MoodSerializer(serializers.HyperlinkedModelSerializer):
     created_by = serializers.ReadOnlyField(source="created_by.username")
-    created_at = serializers.DateTimeField()
 
     class Meta:
         model = Mood
