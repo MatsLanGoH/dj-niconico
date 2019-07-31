0f35a0572c2dc0697d92ada58186835c7ceb3eda
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-08-01 05:40:33 +0900

    Add Mood app
    - also update settings to Black formatting

---
+++ niconico/mood/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MoodConfig(AppConfig):
+    name = "mood"
+    verbose_name = "Mood"
---
+++ niconico/mood/models.py
@@ -0,0 +1,20 @@
+from enum import Enum, auto
+from django.conf import settings
+from django.db import models
+
+
+# Define possible mood choices
+class MoodChoice(Enum):
+    HAPPY = auto()
+    NEUTRAL = auto()
+    SAD = auto()
+
+
+class Mood(models.Model):
+    created_at = models.DateTimeField(auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    mood = models.IntegerField(choices=[(tag, tag.value) for tag in MoodChoice])
+    message = models.CharField(max_length=255)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
+    )
