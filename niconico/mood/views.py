c06521d9dea776f5ade2ceeefd9e3f0e68f54074
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-08-05 05:48:04 +0900

    Fix date handling (again)

--- niconico/mood/views.py
+++ niconico/mood/views.py
@@ -13,21 +13,33 @@ from mood.serializers import MoodSerializer
 
 class MoodViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
-        week = [timezone.now() - timedelta(days=i) for i in range(7)]
-        week_dates = [d.date() for d in week]
-        qs = Mood.objects.filter(created_at__gte=min(week_dates))
+        week = [timezone.localdate() - timedelta(days=i) for i in range(7)]
+
+        qs = Mood.objects.filter(created_at__gte=min(week))
 
         # Find missing dates and create unset moods for them
         # TODO: Rather find a way to handle missing dates on the frontend?
-        valid_days = [m.created_at.date() + timedelta(days=1) for m in qs]
-        missing_days = [d for d in week_dates if d not in valid_days]
+        valid_days = [m.created_at.date() for m in qs]
+        missing_days = [d for d in week if d not in valid_days]
 
         Mood.objects.bulk_create(
-            Mood(created_at=day, mood=MoodChoice.UNSET.value) for day in missing_days
+            Mood(
+                created_at=day,
+                mood=MoodChoice.UNSET.value,
+                message="Auto value for unset day: {}".format(day),
+            )
+            for day in missing_days
         )
 
-        # Retrieve objects once again :(
-        qs = Mood.objects.filter(created_at__gte=min(week_dates))
+        # Retrieve last object per day
+        last_per_day = (
+            Mood.objects.filter(created_at__gte=min(week))
+            .extra(select={"date": "date(created_at)"})
+            .values_list("date")
+            .annotate(max_date=Max("created_at"))
+        )
+        last_dates = [item[1] for item in last_per_day]
+        qs = Mood.objects.filter(created_at__in=last_dates)
         return qs.order_by("created_at")
 
     # Get days for the past week
