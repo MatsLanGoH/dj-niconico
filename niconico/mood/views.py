b55cc5c6d15675596e8263737229b6c0f068d2fb
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-08-16 06:19:52 +0900

    - Create dummy instances instead of db objects for missing Moods

--- niconico/mood/views.py
+++ niconico/mood/views.py
@@ -1,15 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import logging
 from django.utils import timezone
-from django.conf import settings
-from django.db.models import Max, QuerySet
-from django.shortcuts import render
+from django.db.models import Max
 from rest_framework import permissions, viewsets
-from rest_framework.response import Response
 
 from .models import Mood, MoodChoice
-from .permissions import IsOwnerOrReadOnly
 from .serializers import MoodSerializer
 
 logger = logging.getLogger(__name__)
@@ -24,20 +20,25 @@ class MoodViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
     def get_queryset(self):
-        week = [timezone.localtime().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=i) for i in
-                range(7)]
+        week = [
+            timezone.localtime().replace(hour=0, minute=0, second=0, microsecond=0)
+            - timedelta(days=i + 1)
+            for i in range(7)
+        ]
 
         # qs = Mood.objects.filter(created_at__gte=min(week))
         user_mood_qs = self.request.user.moods.all()
         qs = user_mood_qs.filter(created_at__gte=min(week))
 
-        # Find missing dates and create unset moods for them
-        # Hacky solution to avoid duplicate entries created for the latest date
+        # Find missing dates and create unset moods instances for them
         # TODO: Rather find a way to handle missing dates on the frontend?
         valid_days = [m.created_at.date() for m in qs]
-        missing_days = [i for i, d in enumerate(week) if d.date() not in valid_days and i > 0]
+        missing_days = [
+            i for i, d in enumerate(week) if d.date() not in valid_days and i > 0
+        ]
 
-        Mood.objects.bulk_create(
+        # Create filler instances
+        unset_moods = [
             Mood(
                 created_at=timezone.localtime() - timedelta(days=day),
                 mood=MoodChoice.UNSET.value,
@@ -45,16 +46,20 @@ class MoodViewSet(viewsets.ModelViewSet):
                 owner=self.request.user,
             )
             for day in missing_days
-        )
+        ]
 
         # Retrieve last object per day
         last_per_day = (
             self.request.user.moods.all()
-                .filter(created_at__gte=min(week))
-                .extra(select={"date": "date(created_at)"})
-                .values_list("date")
-                .annotate(max_date=Max("created_at"))
+            .filter(created_at__gte=min(week))
+            .extra(select={"date": "date(created_at)"})
+            .values_list("date")
+            .annotate(max_date=Max("created_at"))
         )
         last_dates = [item[1] for item in last_per_day]
         qs = self.request.user.moods.all().filter(created_at__in=last_dates)
-        return qs.order_by("created_at")
+        # return list(qs.order_by("created_at"))
+
+        # Combine actual Moods with filler instances, sort by date and return
+        combined_moods = sorted(list(qs) + unset_moods, key=lambda m: m.created_at)
+        return combined_moods
